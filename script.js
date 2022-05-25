ce316e08536f72fe431642e2277cc4a18125e04f
chigozie <gozieemeafu@gmail.com>
2022-05-25 02:14:13 +0100

    for the site to refresh when reset is clicked

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 
 const btn = document.querySelector(".btn"),
     result = document.querySelector("#result"),
-    todo= document.querySelector(".todo"),
+    todo = document.querySelector(".todo"),
     showTime = document.querySelector(".showTime"),
     reset = document.querySelector(".reset"),
-    arryForTodo=[],
-    arrayForTime=[];
+    arryForTodo = [],
+    arrayForTime = [];
 
 btn.addEventListener("click", whatToDo);
 
@@ -16,24 +16,23 @@ function whatToDo(e) {
     let text = document.querySelector(".txt").value;
     let time = document.querySelector(".time").value;
     // Validate Input
-    if (text === ""|| time === "") {
-        return (result.innerHTML = "Provide a valid information! and refresh");
-    }  else {
-        showResult(`Do "<span>${text}</span>" :`, `by "<span>${time}</span>"\n`, "green","orange");
+    if (text !== "" && time !== "") {
+        showResult(`Do "<span>${text}</span>" :`, `by "<span>${time}</span>"\n`, "green", "orange");
+        reset.style.display = "block";
+    } else {
+        alert("Provide a valid information! and refresh");
     };
-    
-    reset.style.display = "block";
 
     const todoObj = {
         "text": text,
         "time": time
     }
     arryForTodo.push(todoObj);
-   
-    console.log('Array for To-do: ',arryForTodo);
+
+    console.log('Array for To-do: ', arryForTodo);
 }
 
-function showResult(text,time, color,colour) {
+function showResult(text, time, color, colour) {
     todo.style.color = color;
     showTime.style.color = colour;
     todo.innerHTML = text;
